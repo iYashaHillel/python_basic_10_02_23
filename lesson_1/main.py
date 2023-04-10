ad71c4554e941ac10bbbdad77f3f0b7c163b9e82
iYasha <33287747+iYasha@users.noreply.github.com>
2023-04-10 21:32:15 +0300

    10.04

---
+++ lesson_1/main.py
@@ -0,0 +1,32 @@
+
+"""
+bool - Boolean
+int - Integer
+float - Float
+str - String
+"""
+
+
+result = 1 + 2
+print("Hello\n\n\nworld\n", result)
+print(result)
+
+name = input('Enter your name: ')
+print('Hello ' + name)
+print(f'Hello {name}')
+
+
+a = int(input('Enter first number: '))
+b = int(input('Enter second number: '))
+
+res = a + b
+print(f'{a} + {b} = {res}')
+
+# asdjasdaklsdjlaksd
+# asdasdasd
+"""
+Hello
+world
+My first
+programm
+"""
