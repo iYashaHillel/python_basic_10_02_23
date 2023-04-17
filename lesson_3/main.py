3b799b13d18ee10756097156bcb1d669a1960416
iYasha <33287747+iYasha@users.noreply.github.com>
2023-04-17 21:21:39 +0300

    17.04

---
+++ lesson_3/main.py
@@ -0,0 +1,115 @@
+from decimal import Decimal
+import math
+
+
+# print(0.5 + 1)  # 1.5
+# print(0.5 + 0.5)  # 1.0
+# print(0.3 + 0.7)  # 1.0
+# print(Decimal(0.1) + Decimal(0.1) + Decimal(0.1))  # 0.3
+# print(round(0.1 + 0.1 + 0.12, 2))
+#
+# print('Hello w\'orld')
+# print("world")
+
+# True or False
+
+a = 1, 2, 'Text', 2, 2, 2, 3  # tuple
+b = [1, 2, 'Text', 2, 2, 2, 3]  # list
+c = {1, 2, 3, 4, 3, 3, 3, 3, 2, 2, 2, 'Text'}  # set
+d = {'key': 'value', 'key2': 2}  # dict
+# print(a, b, c, d)
+
+# print_output = print('Hello')
+# number = input('Enter your number: ')
+#
+# print(f'Your input: {number}')
+# print(f'Print output: {print_output} | Is None: {print_output == None}')
+
+# float_number = 2.34125125125
+# new_number = round(float_number, 4)
+# print(f'float_number={float_number}, new_number={new_number}')
+# print('Hello', True, False, 1.231, 1111, sep='_---', end='__ENDLINE')
+# print('123', 123, 1, 2, 3, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2)
+#
+# float_number = 'Text'
+# print(float_number)
+# PI = 3.14
+
+# a = 3.1
+# b = 3
+#
+# print(True == 1, False == 0)
+# ASCII
+# UTF-8
+# WindowsCP1256
+
+# print(a > b)
+# print(a < b)
+# print(a >= b)
+# print(a <= b)
+# print(a == b)
+# print(a != b)
+
+# a = 4
+# b = 3
+#
+# print(math.sqrt(4))
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a ** b)
+# print(a % b)
+
+# a = b = 2
+# c, d, e = 1, 2, 3, 'Text'
+# a = 2
+# b = 2
+# c = 1
+# d = 2
+# e = 3
+# print(a, b)
+# print(c, d, e, g)
+
+# a = 4
+# b = a
+#
+# a = 5
+# print(a, b)
+
+
+# a = 5
+#
+# a += 2  # a = a + 2
+# a -= 2  # a = a - 2
+# a *= 2  # a = a * 2
+# a **= 2  # a = a ** 2
+# print(a)
+
+"""
+0 - 20 = Погано
+20 - 40 = Не дуже погано
+40 - 60 = Задовільно
+60 - 80 = Добре
+80 - 100 = Супер
+"""
+mark = 20000
+# mark = input('enter your mark: ')
+
+# print(f'Погано - {mark >= 0 and mark < 20}')
+# print(f'Не дуже погано - {mark >= 20 and mark < 40}')
+# print(f'Задовільно - {mark >= 40 and mark < 60}')
+# print(f'Добре - {mark >= 60 and mark < 80}')
+# print(f'Супер - {mark >= 80 and mark <= 100}')
+#
+# print(True and True and True)
+# print(True or False or False)
+# print(not True)
+
+# print('Hello John!' in 'Hello John!')
+# a = print('123124' not in 'Hello John!')
+#
+# print('Hello John!' is 'Hello John!')
+# print(a is None)
+# print(a is not None)
